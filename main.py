55b6c0481d8b96a868beea2e050f5034d2a0c856
Ernest Wright <ernest.accdep@gmail.com>
2022-09-01 15:12:27 -0400

    it's WORKING

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ if __name__ == '__main__':
 
     application.add_handlers(handlers)  # adding handlers
 
-    application.run_polling()  # we use polling in case we want to test it by running it on our PC
+    # application.run_polling()  # we use polling in case we want to test it by running it on our PC
 
-    # application.run_webhook(  # adding webhooks only if the bot is run on a hosting
-    #     listen="0.0.0.0",
-    #     port=PORT,
-    #     url_path=TOKEN,
-    #     webhook_url='https://kosyachnik-bot.herokuapp.com/' + TOKEN
-    # )
+    application.run_webhook(  # adding webhooks only if the bot is run on a hosting
+        listen="0.0.0.0",
+        port=PORT,
+        url_path=TOKEN,
+        webhook_url='https://kosyachnik-bot.herokuapp.com/' + TOKEN
+    )
