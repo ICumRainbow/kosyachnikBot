0858f9f7388b381df34f43a8c9f7b75503dbb5c5
Ernest Wright <ernest.accdep@gmail.com>
2022-10-06 06:03:46 -0400

    new token for bot because the old one needed to be revoked

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from handlers.stats import stats
 
 PORT = int(os.environ.get('PORT', '8443'))
 # TOKEN = '5431088637:AAHxhERaR-M3IpG7_8l5Fro1mwW95r9riII'  # This API TOKEN IS FOR TESTING - @Testing_bot4testing_bot
-TOKEN = '5715448930:AAG4tgzsitmYx_UOgvqrgdt9Jf36n_Ygp5U'  # This API TOKEN IS FOR PRODUCTION - @kosyachnik_bot
+TOKEN = '5715448930:AAEujLiUY157EJGtdME3ZG8Y6pWF6Ixv0KM'  # This API TOKEN IS FOR PRODUCTION - @kosyachnik_bot
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 
     application.add_handlers(handlers)  # adding handlers
 
-    # application.run_polling()  # we use polling in case we want to test it by running it on our PC
+    application.run_polling()  # we use polling in case we want to test it by running it on our PC
 
     application.run_webhook(  # adding webhooks only if the bot is run on a hosting
         listen="0.0.0.0",
